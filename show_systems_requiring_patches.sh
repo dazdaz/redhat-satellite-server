e60eb9d823b0e53ab1b7e6eccd998ccf8b64c145
dazdaz <darren@dazdaz.org>
2014-06-11 21:58:49 +0200

    Added link to spacecmd

--- show_systems_requiring_patches.sh
+++ show_systems_requiring_patches.sh
@@ -6,6 +6,9 @@
 # The same result is accomplished by using this 1-liner below with xargs
 # spacecmd -u ${user} -p ${pass} system_list | tail -n +1 | xargs --verbose -I {} spacecmd -u ${user} -p ${pass} 
 
+# Requires spacecmd which is not supported by RedHat or installed by default.
+# https://fedorahosted.org/spacewalk/wiki/spacecmd
+
 user="myfirmadmin"
 pass="sssshhhhh"
 tmpfile="/var/tmp/$$.patch"
